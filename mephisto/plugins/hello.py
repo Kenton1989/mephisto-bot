b68b000643f80df133efc60a8b1f61fe9fb8a8df
Kenton <38125586+Kenton1989@users.noreply.github.com>
2022-01-28 00:32:52 +0800

    Update hello.py

--- mephisto/plugins/hello.py
+++ mephisto/plugins/hello.py
@@ -2,6 +2,11 @@ from nonebot import on_command, CommandSession
 
 from nonebot import default_config as cfg
 
+import logging
+
+log = logging.Logger("hello")
+
 @on_command('hello', aliases=('你好'))
 async def hello(session: CommandSession):
+    log.debug("/hello")
     await session.send('你寄吧谁呀')
