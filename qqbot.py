8e00ce0eeaf42fd439140de8b0a6c3ad056a62d8
Kenton <38125586+Kenton1989@users.noreply.github.com>
2022-01-28 01:49:11 +0800

    add warning about missing config

--- qqbot.py
+++ qqbot.py
@@ -1,13 +1,23 @@
 import re
-import nonebot
 from os import path
+import logging
+
+import nonebot
 
 import qqconfig
+
+l = logging.Logger("qqbot")
+
 if __name__ == '__main__':
+
+    if qqconfig.ACCESS_TOKEN == '':
+        l.warn("ACCESS_TOKEN is not set")
+
+    if len(qqconfig.SUPERUSERS):
+        l.warn("SUPERUSERS is not set")
+
     nonebot.init(qqconfig)
-    
-    # nonebot.load_builtin_plugins()
-    
+
     nonebot.load_plugins(
         path.join(path.dirname(__file__), 'mephisto', 'plugins'),
         'mephisto.plugins'
--- qqconfig.py
+++ qqconfig.py
@@ -2,8 +2,10 @@ import re
 
 from nonebot.default_config import *
 
-SUPERUSERS = {1234567890}
+SUPERUSERS = {}
 COMMAND_START = [re.compile(r'[/!／！]')]
+
+ACCESS_TOKEN = ''
 
 HOST = 'localhost'
 PORT = 44444
