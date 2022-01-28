bd7c13cf99ec7f8650459f93b7d6e7aeebb77551
Kenton <38125586+Kenton1989@users.noreply.github.com>
2022-01-29 04:08:18 +0800

    修掉random_echo的严重bug，调小echo概率

--- mephisto/plugins/random_echo.py
+++ mephisto/plugins/random_echo.py
@@ -7,17 +7,16 @@ import aiocqhttp
 import nonebot
 from nonebot import NoneBot
 from nonebot.plugin import PluginManager
+from nonebot.message import CanceledException
 
 log = logging.Logger('random_echo')
 
-echo_prob = 0.8
+echo_prob = 0.02
 
 last_sent = None
 
 @nonebot.message_preprocessor
 async def random_echo(bot: NoneBot, event: aiocqhttp.Event, plugin_manager: PluginManager):
-    log.info("entered random_echo")
-
     if random.random() > echo_prob:
         return
 
@@ -27,18 +26,19 @@ async def random_echo(bot: NoneBot, event: aiocqhttp.Event, plugin_manager: Plug
     msg = event.message
 
     # avoid infinit echo
+    global last_sent
     if msg == last_sent:
         return
 
     # check if msg maybe a command
     msg_str: str = msg.extract_plain_text()
-    for cmd_prefix in bot["COMMAND_START"]:
+    for cmd_prefix in bot.config.COMMAND_START:
         if re.match(cmd_prefix, msg_str):
             return
 
-    log.info("triggered random_echo @[group: %d]", event.group_id)
+    nonebot.logger.info("triggered random_echo @[group: %d]", event.group_id)
     last_sent = msg
 
-    bot.send(event, msg)
+    await bot.send(event, msg)
 
     raise CanceledException("random echo handled")
